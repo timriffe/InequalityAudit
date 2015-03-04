e116835d9c67737698401a58390af4bc0c917eae
Tim Riffe <tim.riffe@gmail.com>
2015-03-04 15:29:24 -0800

    recent as of 2 weeks ago I presume...

--- InequalityAudit/R/FigurePanel.R
+++ InequalityAudit/R/FigurePanel.R
@@ -1,55 +1,139 @@
 setwd("/home/tim/git/InequalityAudit/InequalityAudit")
 
-Results <- local(get(load("Data/Results.Rdata")))
+Results <- local(get(load("/home/tim/Dropbox/Graficos (1)/Results.Rdata")))
+Results2 <- local(get(load("/home/tim/Dropbox/Graficos (1)/Results2.Rdata")))
 
-dim(Results)
-head(Results)
+# cut down to just needed elements of k.
+allk <- sort(unique(Results[,"k"]))
+
+pk <- allk[c(20,40,60,80,100)]
+
 source("R/TernaryTest.R")
 
 rownames(Results) <- NULL
-Results          <-  data.frame(Results)
-Results$col      <- val2col(Results$Corr,n=100)
-w_all 			 <- getTernTriangles(n=50)
-head(Results)
+rownames(Results2) <- NULL
 
-plotk <- function(Results, w_all, .k){
-	w_all$col <- Results$col[Results$k == .k]
-	par(mai=c(.2,.2,.2,.2), xaxs = "i", yaxs = "i", xpd = TRUE)
-	plot(c(-.05,1.05,.5),c(0,0,sqrt(3)/2),type="n", axes = FALSE, 
+Results  <- Results[Results[,"k"] %in% pk, ]
+Results2 <- Results2[Results2[,"k"] %in% pk, ]
+
+
+colbrks <- function(x,breaks,b.pal="YlGnBu"){
+	colRamp <- colorRampPalette(RColorBrewer::brewer.pal(9,b.pal),space="Lab")
+	n <- length(breaks) - 1
+	as.character(cut(
+					x, 
+					breaks = breaks, 
+					labels = colRamp(n),
+					include.lowest = TRUE))
+}
+
+Ncolors <- 100
+breaks <- seq(.6,1,length.out=Ncolors+1)
+
+Results          <- data.frame(Results)
+Results2          <- data.frame(Results2)
+Results$col      <- colbrks(Results$Corr,breaks=breaks,b.pal="YlOrRd")
+Results2$col      <- colbrks(Results2$Corr,breaks=breaks,b.pal="YlOrRd")
+
+w_all 			 <- getTernTriangles(n=50)
+
+
+plotk <- function(Res, w_all, .k){
+	w_all$col <- Res$col[Res$k == .k]
+	par(mai=c(.15,.15,.15,.15), xaxs = "i", yaxs = "i", xpd = TRUE)
+	plot(c(0,1,.5),c(0,0,sqrt(3)/2),type="n", axes = FALSE, 
 			xlab = "", ylab = "", asp = 1, cex = 1.7)
 	plotTernPolygons(w_all)
 	plotTernBorder(border = gray(.7))
 	plotTernLabels(labs=c("E","H","S"))
-	text(.1,sqrt(3)/2,.k,cex=1.7)
+	
+	#text(.1,sqrt(3)/2,.k,cex=1.7)
 }
 graphics.off()
-#dev.new(width=6,height=7)
-getwd()
-pdf("Figures/PanelTest_Corr1.pdf")
-pp <- layout(mat=matrix(c(1:16,17,17,17,17), 5, 4, byrow=TRUE), widths =  c(1.5,1.5,1.5,1.5), c(1.5,1.5,1.5,1.5,1), T)
-#layout.show(pp)
-par(xaxs="i",yaxs="i",oma=c(0,1,0,0))
-k 				<- seq(0,1,length.out = 17)[-1]
+#
 
-for (.k in k){
+#pdf("Figures/PanelCorr.pdf",width=15/2.54,height=21/2.54)
+pdf("/home/tim/Dropbox/Graficos (1)/PanelCorr.pdf",width=15/2.54,height=21/2.54)
+#pdf("Figures/PanelTest_Corr1.pdf")
+# 21 * 29.7
+pp <- layout(mat=matrix(c(1,2,2,2,2,2,1,3,4,5,6,7,1,8,9,10,11,12,1,13,13,13,13,13), 6, 4, byrow=FALSE), 
+		widths =  c(.5,1.5,1.5,1), rep(.5,1.2,1.2,1.2,1.2,1.2),FALSE)
+#layout.show(pp)
+par(xaxs="i",yaxs="i",oma=c(0,0,0,0), mar=c(0,0,0,0))
+plot(NULL, type= "n", xlim=c(0,1),ylim=c(0,1), axes = FALSE, xlab="",ylab="")
+text(c(.25,.6),c(.5,.5),c("CC Poor", "CC Poor II"),cex=1.5)
+
+
+plot(NULL, type= "n", xlim=c(0,1),ylim=c(0,5), axes = FALSE, xlab="",ylab="")
+text(.5,0:4+.7,paste0("k = ",rev(pk)),cex=1.5)
+
+for (.k in pk){
 	plotk(Results, w_all, .k)
 }
-plot(NULL, type="n", xlim = c(0,1),ylim=c(0,1))
-text(.5,.5,"Legend goes here")
+for (.k in pk){
+	plotk(Results2, w_all, .k)
+}
+#plotTernAxes(5,tl=.03, labels=TRUE,cex=.8,off=.08)
+
+legbrks <- seq(.6,1,by=.05)
+coldec <- legbrks[-length(legbrks)] + diff(legbrks) /2
+legcols <- colbrks(coldec,breaks=legbrks,b.pal="YlOrRd")
+
+
+plot(NULL, type="n", xlim = c(0,1),ylim=c(0,1), xlab = "", ylab = "", axes = FALSE)
+yat <- seq(.3,.7,length=9)
+xat <- c(.2,.5)
+x <- seq(0, 1.0, 0.1); y <- c(0,.1,.2,.3,.4,.5,.6,.7,.8,.9,1)
+cbind(x, sprintf("%a", x), sprintf("%a", y))
+rect(xat[1],yat[-9],xat[2],yat[-1],col = legcols,border=NA)
+rect(xat[1],min(yat),xat[2],max(yat),border=gray(.4))
+text(xat[2],yat,sprintf("%.2f",legbrks),pos=4)
+
 dev.off()
+#################################################################################3
 
-dev.new(width=1.5,height=9)
+##################################################################################
+range(c(Results[,"m_avg"],Results2[,"m_avg"]))
+breaks <- seq(0,60,length.out=Ncolors+1)
 
-pp <- layout(mat=matrix(c(1:9), 9,1, byrow=TRUE), widths =  c(rep(1,9)), rep(.9,9), T)
+
+Results$col      <- colbrks(Results$m_avg,breaks=breaks,b.pal="YlOrRd")
+Results2$col      <- colbrks(Results2$m_avg,breaks=breaks,b.pal="YlOrRd")
+graphics.off()
+
+pdf("/home/tim/Dropbox/Graficos (1)/Panelm_avg.pdf",width=15/2.54,height=21/2.54)
+#pdf("Figures/PanelTest_Corr1.pdf")
+# 21 * 29.7
+pp <- layout(mat=matrix(c(1,2,2,2,2,2,1,3,4,5,6,7,1,8,9,10,11,12,1,13,13,13,13,13), 6, 4, byrow=FALSE), 
+		widths =  c(.5,1.5,1.5,1), rep(.5,1.2,1.2,1.2,1.2,1.2),FALSE)
 #layout.show(pp)
-par(xaxs="i",yaxs="i",oma=c(0,1,0,0))
-k 				<- seq(0,1,length.out = 17)[-1]
+par(xaxs="i",yaxs="i",oma=c(0,0,0,0), mar=c(0,0,0,0))
+plot(NULL, type= "n", xlim=c(0,1),ylim=c(0,1), axes = FALSE, xlab="",ylab="")
+text(c(.25,.6),c(.5,.5),c("CC Poor", "CC Poor II"),cex=1.5)
 
-for (.k in k[1:9]){
+
+plot(NULL, type= "n", xlim=c(0,1),ylim=c(0,5), axes = FALSE, xlab="",ylab="")
+text(.5,0:4+.7,paste0("k = ",rev(pk)),cex=1.5)
+
+for (.k in pk){
 	plotk(Results, w_all, .k)
 }
+for (.k in pk){
+	plotk(Results2, w_all, .k)
+}
+#plotTernAxes(5,tl=.03, labels=TRUE,cex=.8,off=.08)
+
+legbrks <- seq(0,60,by=10)
+coldec <- legbrks[-length(legbrks)] + diff(legbrks) /2
+legcols <- colbrks(coldec,breaks=legbrks,b.pal="YlOrRd")
 
 
-#####################################################
-# centile plots
+plot(NULL, type="n", xlim = c(0,1),ylim=c(0,1), xlab = "", ylab = "", axes = FALSE)
+yat <- seq(.3,.7,length=7)
+xat <- c(.2,.5)
 
+rect(xat[1],yat[-7],xat[2],yat[-1],col = legcols,border=NA)
+rect(xat[1],min(yat),xat[2],max(yat),border=gray(.4))
+text(xat[2],yat,sprintf("%.0f",legbrks),pos=4)
+
+dev.off()
--- InequalityAudit/R/TernaryTest.R
+++ InequalityAudit/R/TernaryTest.R
@@ -228,37 +228,42 @@ val2col <- function(values, b.pal = "YlGnBu", n = 30){
 #<<<<<<< HEAD
 #
 ## TODO coordinate Label placement with clockwise/counterclockwise or centered.
-## TODO implement counterclockwise axes
-#plotTernAxes <- function(n = 5, off = .04, tl = .01, clockwise = TRUE, ...){
-#    x <- seq(0, 1, by = 1 / n)
-#    
-#    if (clockwise){
-#        # lower axis, refers to left corner variable
-#        segments(x,0,x+cos(pi/3)*tl,-sin(pi/3)*tl,xpd=TRUE)
-#        text(x+cos(pi/3)*off,-sin(pi/3)*off,rev(x),xpd=TRUE,srt=-60,...)
-#        
-#        # right axis, refers to right corner variable
-#        segments(x/2+1/2,rev(x)*sqrt(3)/2,x/2+1/2+cos(pi/3)*tl,rev(x)*sqrt(3)/2+sin(pi/3)*tl,xpd=TRUE)
-#        text(x/2+1/2+cos(pi/3)*off,rev(x)*sqrt(3)/2+sin(pi/3)*off,x,xpd=TRUE,srt=60,...)
-#        
-#        # left axis, refers to top corner variable
-#        segments(x/2,x*sqrt(3)/2,x/2-tl,x*sqrt(3)/2,xpd=TRUE)
-#        text(x/2-off,x*sqrt(3)/2,x,xpd=TRUE,...)
-#        
-#    } else {
-#        # lower axis, refers to right corner variable
-#        segments(x,0,x-cos(pi/3)*tl,-sin(pi/3)*tl,xpd=TRUE)
-#        text(x-cos(pi/3)*off,-sin(pi/3)*off,x,xpd=TRUE,srt=60,...)
-#        
-#        # right axis, refers to to corner variable
-#        segments(x/2+1/2,rev(x)*sqrt(3)/2,x/2+1/2+tl,rev(x)*sqrt(3)/2,xpd=TRUE)
-#        text(x/2+1/2+off,rev(x)*sqrt(3)/2,rev(x),xpd=TRUE,...)
-#        
-#        # left axis, refers to left corner variable
-#        segments(rev(x)/2,rev(x)*sqrt(3)/2,rev(x)/2-cos(pi/3)*tl,rev(x)*sqrt(3)/2+sin(pi/3)*tl,xpd=TRUE)
-#        text(rev(x)/2-cos(pi/3)*off,rev(x)*sqrt(3)/2+sin(pi/3)*off,x,srt=-60,xpd=TRUE,...) 
-#    }
-#}
+# TODO implement counterclockwise axes
+plotTernAxes <- function(n = 5, off = .04, tl = .01, clockwise = TRUE, labels = FALSE,...){
+    x <- seq(0, 1, by = 1 / n)
+    
+    if (clockwise){
+        # lower axis, refers to left corner variable
+		segments(x,0,x+cos(pi/3)*tl,-sin(pi/3)*tl,xpd=TRUE)
+		if (labels){
+			text(x+cos(pi/3)*off,-sin(pi/3)*off,rev(x),xpd=TRUE,srt=-60,...)
+		}
+		# right axis, refers to right corner variable
+		segments(x/2+1/2,rev(x)*sqrt(3)/2,x/2+1/2+cos(pi/3)*tl,rev(x)*sqrt(3)/2+sin(pi/3)*tl,xpd=TRUE)
+		if (labels){
+			text(x/2+1/2+cos(pi/3)*off,rev(x)*sqrt(3)/2+sin(pi/3)*off,x,xpd=TRUE,srt=60,...)
+		}
+		# left axis, refers to top corner variable
+		segments(x/2,x*sqrt(3)/2,x/2-tl,x*sqrt(3)/2,xpd=TRUE)
+		if (labels){
+			text(x/2-off,x*sqrt(3)/2,x,xpd=TRUE,...)
+		}
+    } else {
+        # lower axis, refers to right corner variable
+        segments(x,0,x-cos(pi/3)*tl,-sin(pi/3)*tl,xpd=TRUE)
+		if (labels){
+        text(x-cos(pi/3)*off,-sin(pi/3)*off,x,xpd=TRUE,srt=60,...)
+	}
+        # right axis, refers to to corner variable
+        segments(x/2+1/2,rev(x)*sqrt(3)/2,x/2+1/2+tl,rev(x)*sqrt(3)/2,xpd=TRUE)
+		if (labels){
+        text(x/2+1/2+off,rev(x)*sqrt(3)/2,rev(x),xpd=TRUE,...)
+        
+        # left axis, refers to left corner variable
+        segments(rev(x)/2,rev(x)*sqrt(3)/2,rev(x)/2-cos(pi/3)*tl,rev(x)*sqrt(3)/2+sin(pi/3)*tl,xpd=TRUE)
+        text(rev(x)/2-cos(pi/3)*off,rev(x)*sqrt(3)/2+sin(pi/3)*off,x,srt=-60,xpd=TRUE,...) 
+    }
+}
 #=======
 #svg("Figures/TernaryMockup.svg")
 #par(mai=c(.4,.4,.4,.4), xaxs = "i", yaxs = "i")
