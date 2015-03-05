55041e0d0a35475ad0a8f2a462954be4c72f730a
Tim Riffe <tim.riffe@gmail.com>
2015-03-05 08:48:52 -0800

    double checked stats, and re-produced table 5

--- InequalityAudit/R/Computations.R
+++ InequalityAudit/R/Computations.R
@@ -48,7 +48,7 @@ w_all 			<- as.list(data.frame(t(w_all)))
 # do a huge computation (better for centile plots)
 # then pick out the values of k that are useful for ternary plots
 #k <- seq(.01,1,by=.01)
-w_iteration <- function(w, DHS, k, CCvar = "CC_Poor2"){
+w_iteration <- function(w, DHS, k, CCvar = "CC_Poor"){
 	
 	# 2) expand w to account for nr of dimensions
 	reps                <- c(2, 2, 6)
@@ -103,7 +103,7 @@ w_iteration <- function(w, DHS, k, CCvar = "CC_Poor2"){
     invisible(cbind(w1 = w[1], w2 = w[2], w3 = w[3], k, Corr = Corr, m_avg = m_out))
 }
 
-Test <- w_iteration(w=c(1,1,1)/3,DHS, k = .3, CCvar = "CC_Poor")
+(Test <- w_iteration(w=c(1,1,1)/3,DHS, k = 1/3, CCvar = "CC_Poor"))
 
 library(parallel)
 
@@ -133,3 +133,62 @@ Results2 <- local(get(load("Results2.Rdata")))
 head(Results)
 mean(Results[,"m_avg"])
 mean(Results2[,"m_avg"])
+
+# stats for Table 5
+w_statsExample <- function(w, DHS, k, CCvar = "CC_Poor"){
+	
+	# 2) expand w to account for nr of dimensions
+	reps                <- c(2, 2, 6)
+	names(reps)         <- c("E", "H", "S")
+	we                  <- unlist(mapply(function(w., r.){
+						rep(w., r.) / r.
+					}, w, reps))
+	Households          <- DHS[,c("E1","E2","H1","H2","S1","S2","S3","S4","S5","S6")]
+	cvec 				<- zapsmall(colSums(t(Households) * we))
+	
+	AF_Poor 			<- outer(cvec,k,">=")
+	colnames(AF_Poor) 	<- k
+	
+# step 6: do poor measures agree?
+	AFCCagree 			<- AF_Poor != DHS[,CCvar]
+	# one-time test:
+	#AFCCagree 			<- AF_Poor != DHS$Test_poor
+	colnames(AFCCagree) <- k
+# a convenience object:
+	DHSvars             <- DHS[, c("country","pes")]
+	
+# 7) get the weighted percent in agreement per country (AF_Poor)
+# for k-dimensional objects
+# data.table idiom preferred for speed
+	agreeGrouping       <- data.table::data.table(cbind(DHSvars, AFCCagree * DHSvars$pes))
+	
+	applycols 			<- c(as.character(k),"pes")
+	Sumsi     			<- agreeGrouping[,lapply(.SD,sum),by=country,.SDcols = applycols]
+	denom               <- unlist(Sumsi[,ncol(Sumsi),with=FALSE]) # we recycle this...
+	mi       			<- 100 * as.matrix(Sumsi[,as.character(k),with=FALSE]) / denom
+	
+# step 8:
+	AFcen        		<- AF_Poor * cvec * DHS$pes
+	CCcen        		<- DHS[,CCvar] * cvec * DHS$pes
+	# a one-time test
+	#CCcen <-  DHS$Test_poor * cvec * DHS$pes
+	P_AFgrouping 		<- data.table::data.table(cbind(DHSvars, AFcen))
+	P_CCgrouping 		<- data.table::data.table(cbind(DHSvars, CCcen))
+	setnames(P_AFgrouping, c(colnames(DHSvars),k))
+	
+	Sumsii     	 		<- P_AFgrouping[,lapply(.SD,sum),by=country,.SDcols = as.character(k)]
+	Sumsiii    	 		<- P_CCgrouping[,lapply(.SD,sum),by=country,.SDcols = "CCcen"]
+	
+	P_AFi        		<- as.matrix(Sumsii[,as.character(k),with=FALSE]) / denom
+	P_CCi        		<- as.matrix(Sumsiii[,"CCcen",with=FALSE]) / denom
+    POut <- data.frame(P_AF = P_AFi, P_CC = P_CCi)
+	rownames(POut) <- Sumsii$country
+	POut
+}
+# for table 5
+CCPoor <- w_statsExample(w=c(1,1,1)/3, DHS, k = 1/3, CCvar = "CC_Poor")
+CCPoor2 <- w_statsExample(w=c(1,1,1)/3, DHS, k = 1/3, CCvar = "CC_Poor2")
+
+Tab5 <- cbind(CCPoor, CCPoor2)
+colnames(Tab5) <- c("P_AF_Poor","P_CC_Poor","P_AF_Poor2","P_CC_Poor2")
+write.table(Tab5,file="Table5.csv",sep=",")
